c2975ccf41dde7eee1410597946471e864d6e529
sero0 <star151214@icloud.com>
2024-03-22 23:41:51 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
+const { User } = require("./models/User")
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://star151214:!!rhddl1021@cluster-dong-001.v9dzqvx.mongodb.net/', ).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
+mongoose.connect(config.mongoURI, ).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
 
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 배가 고프네요')
+})
+
+app.post('/register', async (req, res) => {
+  // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+
+  const user = new User(req.body)
+
+  const result = await user.save().then(()=>{
+    res.status(200).json({
+      success: true
+    })
+  }).catch((err)=>{
+    res.json({ success: false, err })
+  })
+
+
+  // console.log("1")
+  // user.save((err, doc) => {
+  //   if(err) return res.json({success:false, err})
+  //   return res.status(200).json({
+  //     success:true
+  //   })
+  // })
 })
 
 app.listen(port, () => {
